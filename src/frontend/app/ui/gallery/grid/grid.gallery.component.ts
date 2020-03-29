15fd7d52b040cdbd32bc26a875c8ea81deccbebc
Tuur Lievens <tuur.lievens@hotmail.com>
2020-03-29 21:35:30 +0200

    Add hash to location with currently viewing item

--- src/frontend/app/ui/gallery/grid/grid.gallery.component.ts
+++ src/frontend/app/ui/gallery/grid/grid.gallery.component.ts
@@ -76,21 +76,53 @@ const setupLightGallery = (): void => {
       scale: 0.5,
     });
 
-    // Allow back button to close lightbox
+    // Allow back button to close lightbox and push link to actual file
+    const updateHistory = () => {
+      const id = el.getAttribute('lg-uid');
+      const lightbox = window.lgData[id];
+      const filename = lightbox.items[lightbox.index].getAttribute('id');
+      const title = document.title.split(' - ')[0] + ' - ' + filename;
+
+      document.title = title;
+
+      return { lightbox, filename, title };
+    };
     let listener: any = null;
-    el.addEventListener('onBeforeOpen', () => {
-      window.history.pushState('forward', null, window.location.href.split('#')[0] + '#modal');
+    el.addEventListener('onAfterOpen', () => {
+      const { lightbox, filename, title } = updateHistory();
+
+      window.history.pushState('forward', title, window.location.href.split('#')[0] + '#' + filename);
       listener = window.addEventListener('popstate', () => {
-        (window as any).lgData[el.getAttribute('lg-uid')].destroy();
+        lightbox.destroy();
       });
+    }, false);
+
+    el.addEventListener('onAfterSlide', () => {
+      const { filename, title } = updateHistory();
+      document.title = title;
+      window.history.replaceState('forward', title, window.location.href.split('#')[0] + '#' + filename);
     }, false);
 
     el.addEventListener('onBeforeClose', () => {
       if (listener) {
         el.removeEventListener('popstate', listener);
       }
+      document.title = document.title.split(' - ')[0];
       window.history.replaceState('forward', null, window.location.href.split('#')[0]);
     }, false);
+
+    window.addEventListener('beforeunload', () => {
+      document.title = document.title.split(' - ')[0];
+    });
+
+    if (window.location.hash) {
+      const element = document.querySelector(window.location.hash);
+      if (element) {
+        setTimeout(() => {
+          window.scrollTo({ top: element.getBoundingClientRect().top });
+        }, 250);
+      }
+    }
 
     // skip ahead on double touch
     // TODO: Pausing is broken on mobile, no clue why or how to fix it. You can pause it by changing slide for now.
--- src/frontend/app/ui/gallery/grid/photo/photo.grid.gallery.component.ts
+++ src/frontend/app/ui/gallery/grid/photo/photo.grid.gallery.component.ts
@@ -30,7 +30,7 @@ export class GalleryPhotoComponent implements IRenderable, OnInit, OnDestroy {
 
   wasInView: boolean = null;
 
-  videoSelector = "";
+  sourceSelector = '';
   readonly MAX_SEEALLINFO_WIDTH = 800;
 
   constructor(private thumbnailService: ThumbnailManagerService) {
@@ -57,7 +57,7 @@ export class GalleryPhotoComponent implements IRenderable, OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.videoSelector = 'video-' + this.gridMedia.media.name.replace(/[\.\[\]\s,]+/gi, '-');
+    this.sourceSelector = `id${this.gridMedia.media.name.replace(/[\.\[\]\s,#]+/gi, '-')}`;
     this.thumbnail = this.thumbnailService.getThumbnail(this.gridMedia);
     const metadata = this.gridMedia.media.metadata as PhotoMetadata;
     if ((metadata.keywords && metadata.keywords.length > 0) ||
